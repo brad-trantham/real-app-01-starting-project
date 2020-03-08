98bf7de58ae04e991cfdd50d90cb87062aa21532
Brad Trantham <trantham@gmail.com>
2020-03-07 18:10:04 -0600

    Commit after lecture 54

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>Hello!</Text>
+            <Text style={styles.title}>Start a new game!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a number</Text>
+                <TextInput/>
+                <View style={styles.ButtonContainer}>
+                    <Button title="Reset" onPress={() => {}}/>
+                    <Button title="Confirm" onPress={() => {}}/>
+                </View>
+            </View>
         </View>
     );
 }
@@ -14,6 +22,21 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    ButtonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 })
 

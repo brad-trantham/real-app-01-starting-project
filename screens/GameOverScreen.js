989f60fa0964cd950ca02161f7dd358cba6e8f95
Brad Trantham <btrantham@swri.org>
2020-03-19 16:53:58 -0500

    Commit after lecture 75

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import {View, StyleSheet, Button, Image} from 'react-native'
+import {View, Text, StyleSheet, Button, Image} from 'react-native'
 
 import BodyText from '../components/BodyText'
 import TitleText from '../components/TitleText'
+import Colors from '../constants/colors'
 
 const GameOverScreen = props => {
     return(
         <View style={styles.screen}>
             <TitleText>Game Over!</TitleText>
             <View style={styles.imageContainer}>
-                <Image style={styles.image} source={require('../assets/success.png')} resizeMode='cover'/>
+                <Image style={styles.image} 
+                       source={require('../assets/success.png')} 
+                    //    source={{uri: 'https://npinopunintended.files.wordpress.com/2012/05/game-over-1.jpeg'}}
+                    //    // fade duration only applies to the first load, it's cached after that
+                    //    fadeDuration={1000}
+                       resizeMode='cover'/>
             </View>
-            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-            <BodyText>Number was: {props.userNumber}</BodyText>
-            <Button title="NEW GAME" onPress={props.onRestart}/>
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds 
+                to guess the number <Text style={styles.highlight}>{props.userNumber}</Text></BodyText>
+                <Button title="NEW GAME" onPress={props.onRestart}/>
+            </View>
         </View>
     )
 }
@@ -36,6 +44,20 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%'
+    },
+    resultContainer: {
+        marginHorizontal: 30,
+        marginVertical: 15
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    highlight: {
+        // unlike most components in react native, <text> nested inside of <text> will inherit
+        // style from its parent
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold'
     }
 })
 
